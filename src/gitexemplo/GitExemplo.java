3b844e4d58ab09fb1d9e62ca981c442649259616
Kevin Theodoro <kevinttsilva@outlook.com>
2023-08-15 10:51:35 -0300

    Hello World

--- src/gitexemplo/GitExemplo.java
+++ src/gitexemplo/GitExemplo.java
@@ -1,15 +1,7 @@
 package gitexemplo;
 
-/**
- *
- * @author Kevin
- */
 public class GitExemplo {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        
+        System.out.println("Hello world");
     }
 }
